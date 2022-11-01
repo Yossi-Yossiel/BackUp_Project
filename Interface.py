64f7bd69b7e8ba6b4465126696e838fd982b56dc
savtush <aviavichail@gmail.com>
2022-11-01 22:33:08 +0200

    changes

--- Interface.py
+++ Interface.py
@@ -1,30 +1,41 @@
+import threading
 from tkinter import filedialog
 from tkinter import *
 import tkinter
 import tkinter.messagebox
 
+eve = threading.Event
+root = Tk()
 path = ""
 
+
+def label_set(text: str):
+    lbl = Label(root, text=text, relief=RAISED)
+    lbl.pack
+    root.mainloop()
+
+
 def select_folder():
+    global path
     path = ""
     path = filedialog.askdirectory()
     var = "selected directory: " + str(path)
-    lbl = Label(root, textvariable=var, relief=RAISED)
-    return path
+    eve.set
     print(path)
 
 
-def button():
+def button(top):
     B = tkinter.Button(top, text="select a folder", command=select_folder, width=10, height=1)
-    print(B)
-    print("returned")
+    while not eve.is_set:
+        continue
+    print("ee")
     B.pack()
 
+
 def interface():
-    root = Tk()
     root.withdraw()
     top = tkinter.Tk()
-
-    button()
-
+    button(top)
+    print(path)
     top.mainloop()
+interface()
