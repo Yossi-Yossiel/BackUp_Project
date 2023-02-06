52b415f450d4a9f55443bf6d76ddc3ce552b7e22
Aviv Avichail <avivavichail@gmail.com>
2023-02-06 08:35:57 +0200

    started working on the server and started to add sockets

--- ServerProject.py
+++ ServerProject.py
@@ -1,23 +1,44 @@
 import socket
 import random
-def recv_file(client: socket.socket):
-    data =
-    client.send("".encode())
+import sys
+import time
+import mysql.connector
 
+
+def establish_connection(client: socket.socket):
+    rand = random.randint(100, 999)
+    client.send(f"{rand}".encode())
+    data = client.recv(1024).decode()
+    dataArr = data.split(" ")
+    client.send(f"{dataArr[0]} {int(dataArr[1])+1}".encode())
+    data = client.recv(1024)
+    if data == "ok":
+        client.send(b'Please provide the file name and file size')
+        file_name = client.recv(1024).decode()
+        file_size = int(client.recv(1024).decode())
+    else:
+        client.send("connection not stable. disconnecting".encode())
+        client.close()
+
+
+def recv_file(client: socket.socket, file_name: str, file_size: int):
+    data = client.recv(1024)
+    client.send("".encode())
 
 
 def create_file(filename: str, client: socket.socket):
     try:
-        file_open = open(f"{default_filepath}{filename}.7z", "x")
-    except FileExistsError:
+        file_open = open(f"{default_filepath}{filename}.7z", "w")
+    except Exception as e:
+        print(e)
+        sys.exit()
 
 
 default_filepath = "C:/Backup/"
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('0,0,0,0', 5000))
+server_socket.bind(('0.0.0.0', 5000))
 clients = []
 while True:
     server_socket.listen()
     clients.append(server_socket.accept())
-
 
